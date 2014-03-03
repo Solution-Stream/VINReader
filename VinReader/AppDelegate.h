b6524745669d41258e36039381f4e4713304dd8a
stevevilotti <steve.vilotti@solution-stream.com>
2014-03-03 13:23:05 -0500

    Initial Commit

---
+++ VinReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VinReader
+//
+//  Created by Stephen Vilotti on 3/3/14.
+//  Copyright (c) 2014 SolutionStream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VinReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VinReader
+//
+//  Created by Stephen Vilotti on 3/3/14.
+//  Copyright (c) 2014 SolutionStream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
